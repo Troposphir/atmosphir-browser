c19c0567fb76daa430840c7c71cb0a9b2395b706
Leonardo Giovanni Scur <kroltan1@gmail.com>
2018-07-05 22:44:42 -0300

    fix scss, migrate to yarn

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,62 @@
-const {compose} = require("react-app-rewired");
-const {withLoaderOptions: rewireSass} = require("react-app-rewire-sass-modules");
+const path = require("path");
+
+const {compose, getLoader} = require("react-app-rewired");
+
+
+function rewireSass(sassLoaderOptions = {}) {
+    return function(config, env) {
+        const sassExtension = /\.scss/;
+
+        const fileLoader = getLoader(
+            config.module.rules,
+            rule =>
+                rule.loader &&
+                typeof rule.loader === 'string' &&
+                rule.loader.indexOf(`${path.sep}file-loader${path.sep}`) !== -1
+        );
+        fileLoader.exclude.push(sassExtension);
+
+        const cssRules = getLoader(
+            config.module.rules,
+            rule => String(rule.test) === String(/\.css$/)
+        );
+
+        let sassRules;
+        if (env === "production") {
+            sassRules = {
+                test: sassExtension,
+                loader: [
+                    // TODO: originally this part is wrapper in extract-text-webpack-plugin
+                    //       which we cannot do, so some things like relative publicPath
+                    //       will not work.
+                    //       https://github.com/timarney/react-app-rewired/issues/33
+                    ...cssRules.loader,
+                    { loader: require.resolve("sass-loader"), options: sassLoaderOptions }
+                ]
+            };
+        } else {
+            sassRules = {
+                test: sassExtension,
+                use: [
+                    ...cssRules.use,
+                    { loader: require.resolve("sass-loader"), options: sassLoaderOptions }
+                ]
+            };
+        }
+
+        const oneOfRule = config.module.rules.find((rule) => rule.oneOf !== undefined);
+        if (oneOfRule) {
+            oneOfRule.oneOf.unshift(sassRules);
+        }
+        else {
+            // Fallback to previous behaviour of adding to the end of the rules list.
+            config.module.rules.push(sassRules);
+        }
+
+        return config;
+    };
+}
+
 
 module.exports = compose(
     rewireSass({
